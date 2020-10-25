3c0079aeda7d35aab59f9ca50ad3402dc65d2823
Bence-Mr.Chatbee <Bence-Mr.Chatbee@DESKTOP-HDGHG3Q>
2020-10-25 18:33:33 +0100

    3. feladat kész (nem tudom, hogy helyes-e, a feladat leírás nem elég informatív ilyen szempontból)

---
+++ week7/week7/Entities/DeathProbability.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week7.Entities
+{
+    public class DeathProbability
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+
+        
+        public double P { get; set; }
+
+
+    }
+}
